73949071a9235b10b0fdf06f69c141e91762cc96
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-11 13:43:24 +0100

    .

---
+++ demos/vite/src/test.js
@@ -0,0 +1,12 @@
+import * as XLSX from 'xlsx';
+
+const buffer = await fetch('/wc-data.xls').then(r => r.arrayBuffer());
+const workbook = XLSX.read(buffer, { type: 'array' });
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
+const HtmlString = XLSX.utils.sheet_to_html(sheet);
+
+const parser = new DOMParser()
+const doc = parser.parseFromString(HtmlString, "text/html");
+const table = doc.querySelector("table")
+
+console.log(table)
